a5532ebafba7e38d25b57d56f0c23e95a1045f20
wangzhizhong <1017152406@qq.com>
2016-07-11 09:53:32 +0800

    first once

---
+++ array.php
@@ -0,0 +1,18 @@
+<?php
+$contact  = array(
+	'ID' =>1 , 
+	'姓名' =>'吴莫' , 
+	'公司' =>'B公司', 
+	'地址' =>'北京' , 
+	'电话' =>'123456', 
+	'EMAIL' =>'1017152406@qq.com'  
+	);
+
+
+
+ print_r($contact);
+ echo "<br/>";
+ print_r(array_keys($contact));
+ echo "<br/>";
+ print_r(array_values($contact));
+
